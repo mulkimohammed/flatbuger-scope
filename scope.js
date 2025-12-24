bc96130e252d684e47b506a7582657d894fb7b28
Mulki <aweismulki@gmail.com>
2025-12-24 23:00:46 +0300

    initial commit

--- scope.js
+++ scope.js
@@ -1,19 +1,16 @@
 // Write your solution in this file!
-const burgers = ["Classic Cheeseburger", "Chicken Burger"];
-let featuredDrink = "Chochlate Shake";
+const burgers = ["Cheeseburger", "Hamburger"];
+let featuredDrink = "Strawberry Milkshake";
 
 function addBurger() {
   const newBurger = "Flatburger";
   burgers.push(newBurger);
 }
 
-if(true) {
-  const anotherNewBurger = "BBQ Burger";
+if (true) {
+  const anotherNewBurger = "Maple Bacon Burger";
   burgers.push(anotherNewBurger);
-
-  function changeFeaturedDrink() {
-    featuredDrink = "Sparkling Water";
-  }
 }
-     
-  
+function changeFeaturedDrink() {
+  featuredDrink = "The JavaShake";
+}
